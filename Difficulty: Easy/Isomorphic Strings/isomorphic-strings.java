795142c03cf26c507126d23b593499281926e9a9
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-07-19 21:57:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Isomorphic Strings/isomorphic-strings.java
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.*;
+
+
+public class Driver {
+    
+    public static void main(String[] args)throws IOException {
+       
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while(tc-->0)
+        {
+            String s1 = br.readLine();
+            String s2 = br.readLine();
+            
+            Solution obj = new Solution();
+            
+            boolean a = obj.areIsomorphic(s1,s2);
+            if(a)
+            System.out.println(1);
+            else
+            System.out.println(0);
+        }
+    }
+    
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    //Function to check if two strings are isomorphic.
+    public static boolean areIsomorphic(String s,String t)
+    {
+        // Your code here
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Character> mapS = new HashMap<>();
+        Map<Character, Character> mapT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+
+            // Check if there is a conflicting mapping in either of the maps
+            if ((mapS.containsKey(charS) && mapS.get(charS) != charT) || 
+                (mapT.containsKey(charT) && mapT.get(charT) != charS)) {
+                return false;
+            }
+
+            mapS.put(charS, charT);
+            mapT.put(charT, charS);
+        }
+
+        return true;
+        
+    }
+}
