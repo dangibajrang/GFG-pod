de6875df6ceba7e49575fc81a8137b82860db25f
Bajrang Dangi <133575189+dangibajrang@users.noreply.github.com>
2024-09-18 10:20:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Reverse a linked list/reverse-a-linked-list.java
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+class GFG {
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+
+        while (t > 0) {
+            String str[] = read.readLine().trim().split(" ");
+            int n = str.length;
+            Node head = new Node(Integer.parseInt(str[0]));
+            Node tail = head;
+
+            for (int i = 1; i < n; i++) {
+                tail.next = new Node(Integer.parseInt(str[i]));
+                tail = tail.next;
+            }
+
+            Solution ob = new Solution();
+            head = ob.reverseList(head);
+            printList(head);
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// function Template for Java
+
+/* linked list node class:
+
+class Node {
+    int data;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class Solution {
+    // Function to reverse a linked list.
+    Node reverseList(Node head) {
+        // Initialize three pointers: prev, current, and next
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        
+        // Traverse the list
+        while (current != null) {
+            // Store the next node
+            next = current.next;
+            
+            // Reverse the current node's pointer
+            current.next = prev;
+            
+            // Move prev and current one step forward
+            prev = current;
+            current = next;
+        }
+        
+        // At the end, prev will be the new head of the reversed list
+        return prev;
+    }
+}
